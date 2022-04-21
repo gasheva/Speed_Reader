208b6464fb386e015e425b8277d09145b4602bfb
DocGashe <gasheva_99@mail.ru>
2022-04-21 22:04:05 +0500

    feat: add links to mobile sidebar

--- src/components/app/Navigation/TheMobileSidebar.vue
+++ src/components/app/Navigation/TheMobileSidebar.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="mobile-sidebar">
-
+    <ul class="mobile-sidebar__navigation navigation">
+      <router-link v-for="link in links" :key="link.name" :to="link.route" class="navigation-item">
+        <span class="navigation-item__icon icon"></span>
+        <span>{{ t(link.label) }}</span>
+      </router-link>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
+import links from '@/components/app/Navigation/data/index'
+import {useI18n} from "vue-i18n";
+
 export default {
-  name: "TheMobileSidebar"
+  name: "TheMobileSidebar",
+  setup() {
+    const {t} = useI18n();
+
+    return {
+      links,
+      t,
+    };
+  }
 }
 </script>
 
@@ -16,5 +32,14 @@ export default {
   width: 70%;
   height: 100vh;
   background-color: gray;
+
+  &__navigation {
+    flex-direction: column;
+
+  }
+}
+
+.navigation {
+  display: flex;
 }
 </style>
