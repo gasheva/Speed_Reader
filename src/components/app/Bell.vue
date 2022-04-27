64bbfdd39a54deb4f2603208cfed4db64c2a4d1b
DocGashe <gasheva_99@mail.ru>
2022-04-27 20:29:36 +0500

    feat: add bell component

---
+++ src/components/app/Bell.vue
@@ -0,0 +1,47 @@
+<template>
+  <a class="new-msg" href="" @click.prevent>
+    <span v-html="bell"/>
+    <div v-if="msgCount" class="new-msg__count danger">{{ msgCount }}</div>
+  </a>
+</template>
+
+<script>
+import {computed} from "vue";
+import {icons} from "@/constants/icons"
+
+export default {
+  name: "Bell",
+  setup() {
+    const msgCount = computed(() => 4);
+    return {
+      msgCount,
+      bell: icons.bell
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+.new-msg {
+  position: relative;
+  display: inline-block;
+  padding: 0.25rem;
+
+  &__count {
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 1rem;
+    width: 1rem;
+    border-radius: 50%;
+    font-size: 10px;
+    text-align: center;
+    line-height: 1rem;
+  }
+}
+
+.danger {
+  background-color: $danger;
+}
+</style>
