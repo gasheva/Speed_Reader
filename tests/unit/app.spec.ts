b9a51dfceec6e3ab11117b74b5863d06a77ee730
DocGashe <gasheva_99@mail.ru>
2022-05-09 19:14:00 +0500

    test: watch size changing

--- tests/unit/app.spec.ts
+++ tests/unit/app.spec.ts
@@ -7,13 +7,15 @@ import {createStore} from 'vuex';
 import {Breakpoints} from '@/constants/breakpoint.constant';
 import EmptyLayout from '@/components/layouts/EmptyLayout.vue';
 
+const mutations = {
+    setBreakpoint: jest.fn(),
+};
+
 const store = createStore({
     state() {
         return {breakpoint: Breakpoints.XS};
     },
-    mutations: {
-        setBreakpoint: jest.fn(),
-    }
+    mutations
 });
 
 describe('App.vue', () => {
@@ -39,7 +41,7 @@ describe('App.vue', () => {
 
     it('should render empty layout', function () {
         const useRouteMockImplementation = {meta: {layout: 'empty'}};
-        mockUseRoute = jest.spyOn(route, 'useRoute').mockReturnValue(useRouteMockImplementation as unknown as RouteLocationNormalizedLoaded);
+        mockUseRoute = jest.spyOn(route, 'useRoute').mockReturnValueOnce(useRouteMockImplementation as unknown as RouteLocationNormalizedLoaded);
 
         const wrapper = shallowMount(App, {
             global: {
@@ -49,5 +51,28 @@ describe('App.vue', () => {
 
         expect(wrapper.findComponent(MainLayout).exists()).toBe(false);
         expect(wrapper.findComponent(EmptyLayout).exists()).toBe(true);
+    });
+});
+
+describe('App.vue', () => {
+    let mockUseRoute: jest.SpyInstance;
+
+    afterEach(() => {
+        mockUseRoute.mockRestore();
+    });
+
+    it('should commit new breakpoint ', function () {
+        const useRouteMockImplementation = {meta: {layout: 'empty'}};
+        mockUseRoute = jest.spyOn(route, 'useRoute').mockReturnValueOnce(useRouteMockImplementation as unknown as RouteLocationNormalizedLoaded);
+
+        const wrapper = shallowMount(App, {
+            global: {
+                plugins: [store],
+            }
+        });
+
+        // or you can use getter in .vue
+        window = Object.assign(window, {innerWidth: 500});
+        expect(mutations.setBreakpoint.mock.calls).toHaveLength(1);
     });
 });
