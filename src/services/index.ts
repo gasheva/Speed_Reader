824f42414d5beeba2f3a069813cd5f96177037c3
Gasheva Tanya <gasheva_t@cobalt42.ru>
2022-07-04 16:24:21 +0500

    build: add auth basic logic

---
+++ src/services/index.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import store from '@/store'
+
+const auth = axios.create({
+    baseURL: process.env.VUE_APP_AUTH_SERVER
+});
+
+auth.interceptors.request.use(config => {
+    const token = store.getters['auth/getToken'];
+    config.headers!.Authorization = token ? `Bearer ${token}` : '';
+    return config;
+});
+
+auth.interceptors.response.use(undefined, error => {
+    if (401 === error.response.status) {
+        store.dispatch('auth/logout');
+    }
+});
+
+export default auth;
--- src/store/modules/auth.ts
+++ src/store/modules/auth.ts
@@ -1,3 +1,5 @@
+const LS_TOKEN_FIELD_NAME = 'token';
+
 type User = {
     uid: String,
     email: String,
@@ -12,19 +14,34 @@ const state: State = {
     user: undefined,
 };
 
+const getters = {
+    getToken: () => localStorage.getItem(LS_TOKEN_FIELD_NAME),
+};
+
 const mutations = {
     setUser(state: State, user: User) {
         state.user = user;
     },
     clearUser(state: State) {
         state.user = undefined;
+    },
+    setToken(state: State, token: string) {
+        localStorage.setItem(LS_TOKEN_FIELD_NAME, token);
+    },
+    clearToken() {
+        localStorage.removeItem(LS_TOKEN_FIELD_NAME);
     }
 };
 
 const actions = {
     async login({commit}: { commit: Function }) {
         const mockUser = {uid: '1', email: 'beowulf@m.ru', nickname: 'Beowulf'};
+        const mockToken = '123';
         commit('setUser', mockUser);
+        commit('setToken', mockToken);
+    },
+    logout({commit}: { commit: Function }) {
+        commit('clearToken');
     }
 };
 
@@ -32,5 +49,6 @@ export const auth = {
     namespaced: true,
     state,
     mutations,
+    getters,
     actions,
 };
