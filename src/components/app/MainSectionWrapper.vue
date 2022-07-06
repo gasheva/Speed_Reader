119091d873963cd05c5023fe5bf3c0edc9171943
Gasheva Tanya <gasheva_t@cobalt42.ru>
2022-07-06 13:20:03 +0500

    feat: prop to define tiles

--- src/components/app/MainSectionWrapper.vue
+++ src/components/app/MainSectionWrapper.vue
@@ -1,14 +1,43 @@
 <template>
-<div class="section-wrapper">
-    <slot/>
-</div>
+    <div class="section">
+        <div v-if="isVisible('left')"
+             class="section-tile">
+            <slot name="left-tile"/>
+        </div>
+        <div v-if="isVisible('main')"
+             class="section-main">
+            <div class="main-section">
+                <slot/>
+            </div>
+        </div>
+        <div v-if="isVisible('right')"
+             class="section-tile">
+            <slot name="right-tile"/>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
 export default {
-    name: "MainSectionWrapper"
-}
+    name: 'MainSectionWrapper'
+};
 </script>
 <script setup lang="ts">
+
+const props = defineProps({
+    parts: {
+        type: Array,
+        default: ['main'],
+        validator: (val: string[]) => {
+            const values = new Set(['right', 'main', 'left']);
+            return val.reduce((acc: boolean, cur: string) => acc &&= values.has(cur), true);
+        }
+    }
+});
+
+
+const isVisible = (val: string) => {
+    return props.parts?.includes(val);
+};
 
 </script>
