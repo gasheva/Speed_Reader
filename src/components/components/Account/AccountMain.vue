30a7a4d6e8999949a3e974e32d3e6dc3b8b1e0c7
DocGashe <gasheva_99@mail.ru>
2022-07-08 07:32:00 +0300

    feat: account page

---
+++ src/components/components/Account/AccountMain.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="account">
+        <main-section-wrapper :parts="['left', 'main']">
+            <keep-alive>
+                <component
+                    v-if="currentComponentName"
+                    :is="accountComponents[currentComponentName]"/>
+            </keep-alive>
+
+            <template #left-tile>
+                <nav class="account-navigation">
+                    <ul>
+                        <li v-for="link in links">
+                            <span v-html="icons[link.icon]"></span>
+                            <a href="" class="link"
+                               @click.prevent="changeNavigationHandler(link.componentName)"
+                            >{{ t(link.label) }}</a>
+                        </li>
+                    </ul>
+                </nav>
+            </template>
+        </main-section-wrapper>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'AccountMain',
+};
+</script>
+<script setup lang="ts">
+import MainSectionWrapper from "@/components/app/MainSectionWrapper.vue";
+import {links} from "@/components/components/Account/data";
+import {icons} from "@/constants/icons.constants";
+import {ref} from 'vue';
+import {useI18n} from "vue-i18n";
+
+const AccountMainInformation = () => import("@/components/components/Account/AccountMainInformation.vue");
+const AccountMainMessages = () => import("@/components/components/Account/AccountMainMessages.vue");
+
+const {t} = useI18n();
+
+const accountComponents = {AccountMainMessages, AccountMainInformation};
+const currentComponentName = ref<string>('');
+
+const changeNavigationHandler = (componentName: string) => {
+    currentComponentName.value = componentName;
+}
+</script>
+
+<style lang="scss" scoped>
+.account-navigation {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    gap: 1rem;
+}
+</style>
---
+++ src/components/components/Account/AccountMainInformation.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="main-info">
+        Main info
+        <div class="main-info__photo-container">
+            <div class="main-info__photo"></div>
+            <base-button :text="t('loadPhoto')"/>
+        </div>
+        <div class="main-info__credits">
+            <input-text :hint="'E-mail'" v-model:value="credits.email"/>
+            <input-text :hint="'Password'" v-model:value="credits.password"/>
+
+            <input-checkbox :label="t('broadcastAgreement')"
+                            label-side="left"
+                            v-model="credits.toBroadcast"
+            />
+        </div>
+        <base-button :text="t('saveChanges')"/>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'AccountMainInformation',
+};
+</script>
+<script setup lang="ts">
+import BaseButton from "@/components/app/BaseButton.vue";
+import {useI18n} from "vue-i18n";
+import InputText from "@/components/app/InputText.vue";
+import InputCheckbox from "@/components/app/InputCheckbox.vue";
+import {ref} from "vue";
+
+const {t} = useI18n();
+const credits = ref({
+    email: '',
+    password: '',
+    toBroadcast: true,
+});
+
+
+</script>
+<style lang="scss" scoped>
+.main-info {
+
+}
+</style>
