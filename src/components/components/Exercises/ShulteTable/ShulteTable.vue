08af21e7a8a2b1f1bc09a77f29f939be9af0d876
Gasheva Tanya <gasheva_t@cobalt42.ru>
2022-07-13 10:25:17 +0500

    feat: Shulte Table logic

--- src/components/components/Exercises/ShulteTable/ShulteTable.vue
+++ src/components/components/Exercises/ShulteTable/ShulteTable.vue
@@ -1,25 +1,99 @@
 <template>
-<div>
-    <timer-base :start-time="55" @timeout="timeoutHandler"/>
-    shulte
-</div>
+    <div class="exercise">
+        <timer-base :start-time="55" @timeout="timeoutHandler" @timeUpdated="timeUpdatedHandler"/>
+
+        <div class="shulte-table">
+            {{currentVal}}
+            <div class="shulte-table__row" v-for="rowIdx in tableSize">
+                <shulte-table-cell
+                        v-for="colIdx in tableSize"
+                        :value="getVal(rowIdx, colIdx)"
+                        :is-next="getVal(rowIdx, colIdx)===currentVal"
+                        @rightSelect="rightSelectHandler"
+                />
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
 export default {
-    name: "ShulteTable",
-}
+    name: 'ShulteTable',
+};
 </script>
 <script setup lang="ts">
 
 import TimerBase from '@/components/components/Timer/TimerBase.vue';
+import {computed, onBeforeMount, ref} from 'vue';
+import ShulteTableCell from '@/components/components/Exercises/ShulteTable/ShulteTableCell.vue';
 
 const emit = defineEmits(['finish']);
-const timeoutHandler = ()=>{
-    emit('finish');
+const timeoutHandler = () => {
+    onResult();
+};
+
+const onResult = ()=>{
+    const result = {time:currentTimeSec, level: currentVal.value};
+    emit('finish', result);
 }
+
+/* GAME LOGIC */
+let tableSize: number = 5;
+let cellCount: number = 0;
+let shulteTable = ref<number[]>([]);
+let currentVal = ref(1);
+onBeforeMount(async () => {
+    // TODO (fetch size)
+    cellCount = tableSize * tableSize;
+    shulteTable.value = generateTable(cellCount);
+    shuffle(shulteTable.value);
+});
+
+const generateTable = (arrayCapacity: number): number[] => {
+    const randomArray = [];
+    for (let i = 1; i < arrayCapacity + 1; i++) {
+        randomArray.push(i);
+    }
+    return randomArray;
+};
+
+const shuffle = (arr: Object[]): void => {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        const randomIndex = Math.floor(Math.random() * i);
+        [arr[i], arr[randomIndex]] = [arr[randomIndex], arr[i]];
+    }
+};
+
+const getVal = (rowIdx: number, colIdx: number): number => {
+    return shulteTable.value[(rowIdx - 1) * tableSize + (colIdx - 1)];
+};
+
+const isGameFinished = computed(() => {
+    return currentVal.value === cellCount;
+});
+
+const rightSelectHandler = () => {
+    if (isGameFinished.value) {
+        onResult();
+        return;
+    }
+    currentVal.value += 1;
+};
+
+
+/* TIMER LOGIC */
+const startTimeSec = 55;
+let currentTimeSec = startTimeSec;
+const timeUpdatedHandler = (time: number): void => {
+    currentTimeSec = time;
+};
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.shulte-table {
+  &__row {
+    display: flex;
+    width: 100%;
+  }
+}
 </style>
---
+++ src/components/components/Exercises/ShulteTable/ShulteTableCell.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="shulte-table-cell"
+         @click="clickHandler"
+         ref="cellRef"
+         :class="[`shulte-table-cell--${currentStyle}`]"
+    >
+        {{ value }}
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'ShulteTableCell'
+};
+</script>
+<script setup lang="ts">
+import {onBeforeUnmount, ref} from 'vue';
+
+const props = defineProps({
+    value: {type: Number, required: true},
+    isNext: {type: Boolean, required: true},
+});
+
+const emit = defineEmits(['rightSelect', 'wrongSelect']);
+
+const cellRef = ref(null);
+const STYLES = {neutral: 'neutral', right: 'right', wrong: 'wrong'};
+
+const currentStyle = ref<string>(STYLES.neutral);
+
+let timeoutId: number | null = null;
+const setStyle = (style: string) => {
+    currentStyle.value = style;
+    if(timeoutId){
+        resetTimeout();
+    }
+    timeoutId = setTimeout(() => {
+        currentStyle.value = STYLES.neutral;
+        timeoutId = null;
+    }, 500);
+};
+const clickHandler = () => {
+    if (props.isNext) {
+        setStyle(STYLES.right);
+        emit('rightSelect');
+        return;
+    }
+    setStyle(STYLES.wrong);
+    emit('wrongSelect');
+};
+
+const resetTimeout = ()=>{
+    timeoutId && clearTimeout(timeoutId);
+    timeoutId = null;
+}
+onBeforeUnmount(() => {
+    resetTimeout();
+});
+</script>
+
+<style lang="scss" scoped>
+.shulte-table-cell {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 3.5rem;
+  max-width: 3.5rem;
+  min-width: 3.5rem;
+  height: 3.5rem;
+
+  padding: 1rem;
+  border: 1px solid black;
+  color: black;
+  font-size: 1.5rem;
+  transition: color 400ms linear;
+
+  &--neutral {
+    color: black;
+  }
+
+  &--right {
+    color: green;
+  }
+
+  &--wrong {
+    color: red;
+  }
+}
+
+</style>
