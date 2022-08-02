48225ff6b86b29b9731be334260aa1ab577d7ab8
Gasheva Tanya <gasheva_t@cobalt42.ru>
2022-08-02 11:57:04 +0500

    feat: sidebar transition

--- src/components/app/Navigation/TheMobileSidebar.vue
+++ src/components/app/Navigation/TheMobileSidebar.vue
@@ -1,12 +1,17 @@
 <template>
-    <div class="mobile-sidebar">
-        <ul class="mobile-sidebar__navigation navigation">
-            <router-link v-for="link in links" :key="link.name" :to="link.route" class="navigation-item">
-                <span class="navigation-item__icon icon"></span>
-                <span>{{ t(link.label) }}</span>
-            </router-link>
-        </ul>
-    </div>
+    <Transition name="slide">
+        <div class="mobile-sidebar">
+            <ul class="mobile-sidebar__navigation navigation">
+                <router-link v-for="link in links" :key="link.name"
+                             :to="link.route"
+                             @click="hideSidebar"
+                             class="navigation-item">
+                    <span class="navigation-item__icon icon"></span>
+                    <span>{{ t(link.label) }}</span>
+                </router-link>
+            </ul>
+        </div>
+    </Transition>
 </template>
 
 <script lang="ts">
@@ -18,26 +23,41 @@ export default {
 <script setup lang="ts">
 import {useI18n} from 'vue-i18n';
 import links from '@/components/app/Navigation/data/index';
+import {useStore} from 'vuex';
 
 const {t} = useI18n();
+const store = useStore();
 
+const hideSidebar = () => {
+    store.commit('setSidebarVisible', false);
+};
 </script>
 
 <style lang="scss" scoped>
 .mobile-sidebar {
-    position: absolute;
-    width: 70%;
-    height: 100vh;
-    background-color: gray;
-    z-index: 1;
+  position: absolute;
+  width: 70%;
+  height: 100vh;
+  background-color: gray;
+  z-index: 1;
 
-    &__navigation {
-        flex-direction: column;
-
-    }
+  &__navigation {
+    flex-direction: column;
+  }
 }
 
 .navigation {
-    display: flex;
+  display: flex;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transform: translateX(0);
+  transition: all 500ms linear;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateX(-100%);
 }
 </style>
