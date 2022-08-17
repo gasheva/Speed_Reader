fb2edb885fbbd63dfee8aa1a5a9364c20f37525d
Gasheva Tanya <gasheva_t@cobalt42.ru>
2022-08-17 10:04:52 +0500

    build: fix service workers registration build: setup vue env variables

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import router from './router';
 import store from './store';
 import i18n from './i18n';
 
+import './registerServiceWorker'
 import '@/assets/scss/style.scss';
 
-createApp(App).use(i18n).use(store).use(router).mount('#app');
+const app = createApp(App).use(i18n).use(store).use(router);
+if (process.env.NODE_ENV === 'development') {
+    app.config.performance = true;
+}
+app.mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
 const {defineConfig} = require('@vue/cli-service');
+const webpack = require('webpack');
+
 module.exports       = defineConfig({
     transpileDependencies: true,
+    //You can get info in this file: node_modules/@vue/cli-service/lib/config/base.js
+    chainWebpack: config =>
+        config
+            .plugin('feature-flags')
+            .use(require('webpack').DefinePlugin, [{
+                __VUE_OPTIONS_API__: 'false',
+                __VUE_PROD_DEVTOOLS__: 'true'   // TODO
+            }]),
     css:                   {
         loaderOptions: {
             sass: {
@@ -20,4 +30,15 @@ module.exports       = defineConfig({
             fullInstall:     true,
         },
     },
+
+    pwa:{
+        name: "SpeedReading",
+        icons: {
+            favicon32: 'img/icons/favicon-32x32.png',
+            favicon16: 'img/icons/favicon-16x16.png',
+            appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+            maskIcon: 'img/icons/safari-pinned-tab.svg',
+            msTileImage: 'img/icons/msapplication-icon-144x144.png'
+        }
+    }
 });
