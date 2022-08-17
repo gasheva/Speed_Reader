cfa5f1999eac7db7de5d101fa797ce8ded846cdd
Gasheva Tanya <gasheva_t@cobalt42.ru>
2022-08-17 11:04:52 +0500

    feat: add icons

--- vue.config.js
+++ vue.config.js
@@ -36,9 +36,8 @@ module.exports       = defineConfig({
         icons: {
             favicon32: 'img/icons/favicon-32x32.png',
             favicon16: 'img/icons/favicon-16x16.png',
-            appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
-            maskIcon: 'img/icons/safari-pinned-tab.svg',
-            msTileImage: 'img/icons/msapplication-icon-144x144.png'
+            appleTouchIcon: 'img/icons/apple-touch-icon.png',
+            msTileImage: 'img/icons/mstile-150x150.png'
         }
     }
 });
